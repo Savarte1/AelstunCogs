4a35f8e76a973f4ae2283516ee64533e713797c5
TheCensorius <artevenia@gmail.com>
2022-09-11 05:28:15 -0700

    Added code to PollPin cog

--- PollPin/__init__.py
+++ PollPin/__init__.py
@@ -1,5 +1,4 @@
 from .pollpin import PollPin
 
-
 def setup(bot):
     bot.add_cog(PollPin(bot))
--- PollPin/pollpin.py
+++ PollPin/pollpin.py
@@ -1,13 +1,33 @@
 from redbot.core import commands
+from redbot.core import Config
+from redbot.core.bot import Red
+import discord
 
 class PollPin(commands.Cog):
-    """A cog to allow pins for voting ballots using forms"""
+    """A cog to generate pins for voting ballots using forms"""
 
-    def __init__(self, bot):
+    def __init__(self, bot: Red):
         self.bot = bot
+        self.config = Config.get_conf(self, identifier=46_930_395_012, force_registration=True)
+        default_member = {
+            "pincodes": {}
+        }
+        default_guild = {
+            "polls": {}
+        }
+        self.config.register_member(**default_member)
+        self.config.register_guild(**default_guild)
 
     @commands.command()
-    async def mycom(self, ctx):
-        """This does stuff!"""
-        # Your code will go here
-        await ctx.send("I can do stuff!")
+    async def getpin(self, ctx: commands.Context, pollname: str):
+        """Obtain a poll pin"""
+        
+        await ctx.author.send("Your PIN for the " + pollname + "poll is")
+    
+    @commands.command()
+    @commands.mod
+    async def mkpoll(self, ctx: commands.Context, pollname: str, role: discord.Role):
+        """Make new poll"""
+        async with self.config.guild(ctx.guild).polls() as polls:
+            polls[pollname] = {"role": role.id, "pins": {}}
+        await ctx.send("Made new poll " + pollname + "for role" + role.name)
